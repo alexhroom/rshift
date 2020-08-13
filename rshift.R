12d2352d7991f2b42fdf8c654ad69d1011f14a9c
alexhroom <69592136+alexhroom@users.noreply.github.com>
2020-08-13 16:24:57 +0100

    Update rshift.R

--- rshift.R
+++ rshift.R
@@ -108,7 +108,7 @@ RSI_graph <- function(data, col, time, rsi){
   #creates two graphs, one of data and one of the RSI,
   #as seen in Rodionov (2004)
   
-  #
+  #while RSI() requires quotes, this doesn't work if you put quotes around col or time
   p1 <- ggplot(data) + geom_col(aes(x = {{ time }}, y = {{ col }}))
   p2 <- ggplot(data) + geom_col(aes(x = {{ time }}, y = {{ rsi }}))
   grid::pushViewport(grid::viewport(layout = grid::grid.layout(2, 1)))
