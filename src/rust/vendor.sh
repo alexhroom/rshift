868d3beb90c2cebb726a792c32dc0c51d74a7e03
alexhroom <alex.room@btinternet.com>
2023-08-14 16:18:53 +0100

    hopefully fixed vendor

--- src/rust/vendor.sh
+++ src/rust/vendor.sh
@@ -1,15 +1,5 @@
 #!/bin/sh -e
-
+rm -Rf vendor vendor.tar.xz
 cargo vendor
-
-# c.f. https://reproducible-builds.org/docs/archives/
-tar \
-  --sort=name \
-  --mtime='1970-01-01 00:00:00Z' \
-  --owner=0 \
-  --group=0 \
-  --numeric-owner \
-  --xz \
-  --create \
-  --file=vendor.tar.xz \
-  vendor
+tar -cJ --no-xattrs -f vendor.tar.xz vendor
+rm -Rf vendor
