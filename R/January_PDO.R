e8627f6aeb5b1955910c601661e4860d820c4cdf
alexhroom <alexhroom@dell-xps>
2023-10-17 17:16:18 +0100

    roxygenised new data

---
+++ R/January_PDO.R
@@ -0,0 +1,16 @@
+#' Pacific Decadal Oscillation in January
+#'
+#' A dataset containing January PDO values. Subset of the data
+#' from NOAA: <https://www.ncei.noaa.gov/access/monitoring/pdo/>.
+#'
+#' \itemize{
+#'   \item PDO - Pacific Decadal Oscillation in January for the given year.
+#'   \item Age - the year for which the PDO was measured.
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name January_PDO
+#' @usage data(January_PDO)
+#' @format A data frame with 104 rows and 2 variables
+NULL
